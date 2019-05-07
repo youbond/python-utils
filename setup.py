516a22ad71685dc33c88a095d6bac4d0b719e8ae
resley <resley.rodrigues@originmarkets.com>
2019-05-07 11:08:55 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='origin_common',
-    version='0.0.1',
+    version='0.0.2',
     description='Common util functions and mixins to make development of other projects easier.',
     url='https://git.originmarkets.com:48201/originmarkets/common',
     packages=['origin_common'],
