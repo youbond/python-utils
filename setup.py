4300409112b839cebf1a0e0828cfee9e42b52974
resley <resley.rodrigues@originmarkets.com>
2019-11-28 12:32:49 +0000

    [BAPT-7709] Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="origin_common",
-    version="0.0.2",
+    version="0.0.3",
     description="Common util functions and mixins to make development "
     "of other projects easier.",
     url="https://git.originmarkets.com:48201/originmarkets/origin-common",
