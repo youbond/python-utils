9820013edab48aac022f3cb21bb5247c4d24497d
resley <resley.rodrigues@originmarkets.com>
2019-11-28 14:13:31 +0000

    [BAPT-7709] Fix packages

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     description="Common util functions and mixins to make development "
     "of other projects easier.",
     url="https://git.originmarkets.com/originmarkets/origin-common",
-    packages=find_packages(exclude=["*.tests*"]),
+    # packages=find_packages(exclude=["*.tests*"]),
+    packages=["origin_common", "origin_common.constants"],
     zip_safe=True,
     tests_require=["tox"],
+    python_requires=">=3.5",
 )
