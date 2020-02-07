f44f3f2af9bf59b45b9beb2f4bb0ed816ae88392
resley <resley.rodrigues@originmarkets.com>
2020-02-07 14:09:22 +0000

    [BAPT-8016] Use currency code as it's label as well

--- origin_common/constants/currencies.py
+++ origin_common/constants/currencies.py
@@ -1,12 +1,12 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 from origin_common.constants.base import Constant, Constants
 
 
 class Currency(Constant[str]):
-    def __init__(self, value: str, label: str, symbol: str = None, is_g10=False):
-        label = "{} ({})".format(label, value)
-        super().__init__(value, label)
+    def __init__(self, value: str, name: str, symbol: str = None, is_g10=False):
+        super().__init__(value, value)
+        self.name = "{} ({})".format(name, value)
         if symbol is None:
             symbol = value
         self.symbol = symbol
@@ -19,227 +19,227 @@ class Currency(Constant[str]):
 
 
 class Currencies(Constants[Currency]):
-    USD = Currency(value="USD", label="US Dollar", symbol="$", is_g10=True,)
-    EUR = Currency(value="EUR", label="Euro", symbol="€", is_g10=True,)
-    GBP = Currency(value="GBP", label="Pound Sterling", symbol="£", is_g10=True,)
-    JPY = Currency(value="JPY", label="Yen", symbol="¥", is_g10=True,)
+    USD = Currency(value="USD", name="US Dollar", symbol="$", is_g10=True, )
+    EUR = Currency(value="EUR", name="Euro", symbol="€", is_g10=True, )
+    GBP = Currency(value="GBP", name="Pound Sterling", symbol="£", is_g10=True, )
+    JPY = Currency(value="JPY", name="Yen", symbol="¥", is_g10=True, )
     CHF = Currency(
         value="CHF",
-        label="Swiss Franc",
+        name="Swiss Franc",
         # symbol="₣",
         is_g10=True,
     )
     AUD = Currency(
         value="AUD",
-        label="Australian Dollar",
+        name="Australian Dollar",
         # symbol="$",
         is_g10=True,
     )
     NZD = Currency(
         value="NZD",
-        label="New Zealand Dollar",
+        name="New Zealand Dollar",
         # symbol="$",
         is_g10=True,
     )
     CAD = Currency(
         value="CAD",
-        label="Canadian Dollar",
+        name="Canadian Dollar",
         # symbol="$",
         is_g10=True,
     )
     SEK = Currency(
         value="SEK",
-        label="Swedish Krona",
+        name="Swedish Krona",
         # symbol="kr",
         is_g10=True,
     )
     NOK = Currency(
         value="NOK",
-        label="Norwegian Krone",
+        name="Norwegian Krone",
         # symbol="kr",
         is_g10=True,
     )
 
     HKD = Currency(
         value="HKD",
-        label="Hong Kong Dollar",
+        name="Hong Kong Dollar",
         # symbol="$",
     )
     AED = Currency(
         value="AED",
-        label="UAE Dirham",
+        name="UAE Dirham",
         # symbol="د.إ",
     )
     AMD = Currency(
         value="AMD",
-        label="Armenian Dram",
+        name="Armenian Dram",
         # symbol="Դ",
     )
     ARS = Currency(
         value="ARS",
-        label="Argentine Peso",
+        name="Argentine Peso",
         # symbol="$",
     )
     BRL = Currency(
         value="BRL",
-        label="Brazilian Real",
+        name="Brazilian Real",
         # symbol="R$",
     )
     CLP = Currency(
         value="CLP",
-        label="Chilean peso",
+        name="Chilean peso",
         # symbol="$",
     )
     CNH = Currency(
         value="CNH",  # same as CNY
-        label="Yuan Renminbi (Offshore)",
+        name="Yuan Renminbi (Offshore)",
         # symbol="¥",
     )
     CNY = Currency(
         value="CNY",
-        label="Yuan Renminbi",
+        name="Yuan Renminbi",
         # symbol="¥",
     )
     COP = Currency(
         value="COP",
-        label="Colombian peso",
+        name="Colombian peso",
         # symbol="$",
     )
     CZK = Currency(
         value="CZK",
-        label="Czech Koruna",
+        name="Czech Koruna",
         # symbol="Kč",
     )
     DKK = Currency(
         value="DKK",
-        label="Danish Krone",
+        name="Danish Krone",
         # symbol="kr",
     )
     EEK = Currency(
         value="EEK",
-        label="Estonian Kroon",
+        name="Estonian Kroon",
         # symbol="kr",
     )
     GEL = Currency(
         value="GEL",
-        label="Lari",
+        name="Lari",
         # symbol="ლ",
     )
     HUF = Currency(
         value="HUF",
-        label="Forint",
+        name="Forint",
         # symbol="Ft",
     )
     IDR = Currency(
         value="IDR",
-        label="Rupiah",
+        name="Rupiah",
         # symbol="Rp",
     )
     INR = Currency(
         value="INR",
-        label="Indian Rupee",
+        name="Indian Rupee",
         # symbol="₹",
     )
     ISK = Currency(
         value="ISK",
-        label="Iceland Krona",
+        name="Iceland Krona",
         # symbol="Kr",
     )
-    KGS = Currency(value="KGS", label="Som",)
-    KRW = Currency(value="KRW", label="Won", symbol="₩",)
+    KGS = Currency(value="KGS", name="Som", )
+    KRW = Currency(value="KRW", name="Won", symbol="₩", )
     KZT = Currency(
         value="KZT",
-        label="Tenge",
+        name="Tenge",
         # symbol="〒",
     )
     MXN = Currency(
         value="MXN",
-        label="Mexican peso",
+        name="Mexican peso",
         # symbol="$",
     )
     MYR = Currency(
         value="MYR",
-        label="Malaysian Ringgit",
+        name="Malaysian Ringgit",
         # symbol="RM",
     )
     NTD = Currency(
         value="NTD",  # real one is TWD
-        label="New Taiwan dollar",
+        name="New Taiwan dollar",
         # symbol="NT$",
     )
     PEN = Currency(
         value="PEN",
-        label="Peruvian Sol",
+        name="Peruvian Sol",
         # symbol="S/.",
     )
     PHP = Currency(
         value="PHP",
-        label="Philippine Peso",
+        name="Philippine Peso",
         # symbol="₱",
     )
     PLN = Currency(
         value="PLN",
-        label="Zloty",
+        name="Zloty",
         # symbol="zł",
     )
     QAR = Currency(
         value="QAR",
-        label="Qatari Rial",
+        name="Qatari Rial",
         # symbol="ر.ق",
     )
     RON = Currency(
         value="RON",
-        label="Romanian Leu",
+        name="Romanian Leu",
         # symbol="L",
     )
     RSD = Currency(
         value="RSD",
-        label="Serbian Dinar",
+        name="Serbian Dinar",
         # symbol="din",
     )
     RUB = Currency(
         value="RUB",
-        label="Russian Ruble",
+        name="Russian Ruble",
         # symbol="р.",
     )
     SGD = Currency(
         value="SGD",
-        label="Singapore Dollar",
+        name="Singapore Dollar",
         # symbol="$",
     )
     SKK = Currency(
         value="SKK",
-        label="Slovak Koruna",
+        name="Slovak Koruna",
         # symbol="Sk",
     )
     TRY = Currency(
         value="TRY",
-        label="Turkish Lira",
+        name="Turkish Lira",
         # symbol="₤",
     )
     UAH = Currency(
         value="UAH",
-        label="Hryvnia",
+        name="Hryvnia",
         # symbol="₴",
     )
     UYU = Currency(
         value="UYU",
-        label="Uruguayan peso",
+        name="Uruguayan peso",
         # symbol="$",
     )
     VND = Currency(
         value="VND",
-        label="Dong",
+        name="Dong",
         # symbol="₫",
     )
     ZAR = Currency(
         value="ZAR",
-        label="Rand",
+        name="Rand",
         # symbol="R",
     )
     ZMK = Currency(
         value="ZMK",  # real one is ZMW
-        label="Zambian Kwacha",
+        name="Zambian Kwacha",
         # symbol="K",
     )
 
@@ -247,6 +247,9 @@ class Currencies(Constants[Currency]):
         if only_g10:
             return tuple((attr.value, attr.label) for attr in self if attr.is_g10)
         return super().to_django_choices()
+
+    def get_name(self, value: Union[str, Currency]) -> str:
+        return self[value].name
 
 
 CURRENCIES = Currencies()
